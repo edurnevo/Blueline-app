6bda2f7ac993eefcf9ffc8506918061365ae11c4
edurnevo <edurnevila@gmail.com>
2020-12-31 13:43:19 +0100

    Set up components

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+// import "./stylesheet/index.scss";
+import logo from "../images/WingsLogo.jpeg";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Blueline
+        </p>
+      
+      </header>
+    </div>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from "./components/App";
+import "./stylesheet/index.scss";
+import { HashRouter } from "react-router-dom";
+import reportWebVitals from './components/reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <HashRouter>
     <App />
-  </React.StrictMode>,
+  </HashRouter>,
   document.getElementById('root')
 );
 
