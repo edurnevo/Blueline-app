24e6acf5f4861b9e39e4825463d3b7873b2ba4a1
edurnevo <edurnevila@gmail.com>
2020-12-31 15:33:43 +0100

    Log in

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,18 @@
 import React from "react";
-
+import logo from "../images/WingsLogo.jpeg";
+import hamburgerLink from "../images/ico-menu.svg";
+//import "../stylesheet/App.scss";
 
 function App() {
-  return <div>Hola mundo</div>;
-
+  return ( 
+    <>
+  <a className="hamburgerLink" target="_blank"><img src={hamburgerLink} alt="Menú"/></a>
+  <div>
+    <img className="logo" src={logo} alt="logo"/>
+  </div>
+  <button type="button">Log in</button>
+  </>
+  );
 }
 
 export default App;
