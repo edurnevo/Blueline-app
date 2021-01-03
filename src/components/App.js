e0ca3fb75e726cff7150805312d4919781581000
edurnevo <edurnevila@gmail.com>
2021-01-03 19:04:22 +0100

    Add landing

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,17 @@
 import React from "react";
 import logo from "../images/WingsLogo.jpeg";
 import hamburgerLink from "../images/ico-menu.svg";
-//import "../stylesheet/App.scss";
+import "../stylesheet/App.scss";
 
 function App() {
   return ( 
-    <>
-  <a className="hamburgerLink" target="_blank"><img src={hamburgerLink} alt="Menú"/></a>
-  <div>
-    <img className="logo" src={logo} alt="logo"/>
-  </div>
-  <button type="button">Log in</button>
-  </>
+      <div className="landing">
+        <a className="landing_hamburgerLink" target="_blank"><img className="landing_hamburgerLink_lines" src={hamburgerLink} alt="Menú"/></a>
+        <img className="landing_logo" src={logo} alt="logo"/>
+        <button className="landing_btn" type="button">Log in</button>
+      </div>
   );
 }
 
 export default App;
+  
